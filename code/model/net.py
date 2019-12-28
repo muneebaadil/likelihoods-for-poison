ee2a83c6bb4bf57551fabc4db561e9a122bf772b
Muhammad Yaseen <mohammadyaseen13@gmail.com>
2019-12-28 17:41:41 +0100

    added with single optimizer

--- code/model/net.py
+++ code/model/net.py
@@ -10,43 +10,36 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.use_lgm = use_lgm
 
-        self.base.conv1_1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
-        self.base.prelu1_1 = nn.PReLU()
-        self.base.conv1_2 = nn.Conv2d(32, 32, kernel_size=5, padding=2)
-        self.base.prelu1_2 = nn.PReLU()
-        self.base.conv2_1 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
-        self.base.prelu2_1 = nn.PReLU()
-        self.base.conv2_2 = nn.Conv2d(64, 64, kernel_size=5, padding=2)
-        self.base.prelu2_2 = nn.PReLU()
-        self.base.conv3_1 = nn.Conv2d(64, 128, kernel_size=5, padding=2)
-        self.base.prelu3_1 = nn.PReLU()
-        self.base.conv3_2 = nn.Conv2d(128, 128, kernel_size=5, padding=2)
-        self.base.prelu3_2 = nn.PReLU()
-        self.base.preluip1 = nn.PReLU()
-        self.base.ip1 = nn.Linear(128 * 3 * 3, 2)
-
-        if not self.use_lgm:
-            print("using ip2")
-            self.base.ip2 = nn.Linear(2, 10)
+        modules = [
+            nn.Conv2d(1, 32, kernel_size=5, padding=2),
+            nn.PReLU(),
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.PReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=5, padding=2),
+            nn.PReLU(),
+            nn.Conv2d(64, 64, kernel_size=5, padding=2),
+            nn.PReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=5, padding=2),
+            nn.PReLU(),
+            nn.Conv2d(128, 128, kernel_size=5, padding=2),
+            nn.PReLU(),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.PReLU(),
+            nn.Linear(128 * 3 * 3, 2)
+        ]
 
         if self.use_lgm:
-            self.lgm.lgm1 = LGMLoss_v0(num_classes, feat_dim, alpha)
+            #pass
+            self.lgm = LGMLoss_v0(num_classes, feat_dim, alpha)
+        else:
+            modules.append(nn.Linear(2, 10))
 
+        self.base = nn.Sequential(*modules)
 
     def forward(self, x):
 
-        x = self.base.prelu1_1(self.base.conv1_1(x))
-        x = self.base.prelu1_2(self.base.conv1_2(x))
-        x = F.max_pool2d(x, 2)
-        x = self.base.prelu2_1(self.base.conv2_1(x))
-        x = self.base.prelu2_2(self.base.conv2_2(x))
-        x = F.max_pool2d(x, 2)
-        x = self.base.prelu3_1(self.base.conv3_1(x))
-        x = self.base.prelu3_2(self.base.conv3_2(x))
-        x = F.max_pool2d(x, 2)
-        x = x.view(-1, 128 * 3 * 3)
-        ip1 = self.base.preluip1(self.base.ip1(x))
-        ip2 = self.base.ip2(ip1) if not self.use_lgm else None
-
-        return ip2, ip1
-        # return None, ip1
+        ip1 = self.base(x)
+        return ip1
--- code/train_mnist_LGM.py
+++ code/train_mnist_LGM.py
@@ -6,14 +6,16 @@ from torch.autograd import Variable
 from torchvision import datasets
 from torch.utils.data import DataLoader
 import torch.optim.lr_scheduler as lr_scheduler
+import argparse
+
 from model.net import Net
-#from model.lgm import LGMLoss_v0, LGMLoss
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 def visualize(feat, labels, epoch):
+
     plt.ion()
     c = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff',
          '#ff00ff', '#990000', '#999900', '#009900', '#009999']
@@ -25,11 +27,11 @@ def visualize(feat, labels, epoch):
     plt.ylim(ymin=-7,ymax=7)
     plt.text(-4.8, 4.6, "epoch=%d" % epoch)
     plt.savefig('./images/LGM_loss_epoch=%d.jpg' % epoch)
-    # plt.draw()
-    # plt.pause(0.001)
+
     plt.close()
 
 def test(test_loder, criterion, model, use_cuda):
+
     correct = 0
     total = 0
     for i, (data, target) in enumerate(test_loder):
@@ -57,25 +59,16 @@ def train(train_loader, model, criterion, optimizer, epoch, loss_weight, use_cud
             data = data.cuda()
             target = target.cuda()
 
-        # TODO: doesn't make sense to have these as Vars ???
-        #data, target = Variable(data), Variable(target)
-
-        _, feats = model(data)
-        logits, mlogits, likelihood = model.lgm(feats, target)
+        feats = model(data)
+        logits, mlogits, likelihood = model.lgm(feat=feats, label=target)
         loss = criterion(mlogits, target) + loss_weight * likelihood
 
         _, predicted = torch.max(logits.data, 1)
         accuracy = (target.data == predicted).float().mean()
 
         optimizer.zero_grad()
-        #optimizer[0].zero_grad()
-        #optimizer[1].zero_grad()
-
         loss.backward()
-
         optimizer.step()
-        #optimizer[0].step()
-        #optimizer[1].step()
 
         ip1_loader.append(feats)
         idx_loader.append((target))
@@ -90,12 +83,8 @@ def train(train_loader, model, criterion, optimizer, epoch, loss_weight, use_cud
 
 
 def main():
-    #if torch.cuda.is_available():
-    #    use_cuda = True
-    #else:
-    #    use_cuda = False
 
-    use_cuda = True
+    use_cuda = False
     batch_size = 64
 
     # Dataset
@@ -109,43 +98,25 @@ def main():
         transforms.Normalize((0.1307,), (0.3081,))]))
     test_loader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=4)
 
-    # Model
-    model = Net()
-
-    # NLLLoss
+    model = Net(use_lgm=True)
     criterion = nn.CrossEntropyLoss()
-    #nllloss = nn.CrossEntropyLoss()
-    # CenterLoss
     loss_weight = 0.1
-    #lgm_loss = LGMLoss_v0(10, 2, 1.0)
 
     if use_cuda:
         criterion = criterion.cuda()
-        #nllloss = nllloss.cuda()
-        #lgm_loss = lgm_loss.cuda()
         model = model.cuda()
-
-    #criterion = [nllloss, lgm_loss]
 
     optimizer = optim.SGD([
         {'params': model.base.parameters(), 'lr': 0.001, 'momentum': 0.9, 'weight_decay': 0.0005},
         {'params': model.lgm.parameters(),'lr': 0.1}
     ], lr=0.001, momentum=0.9, weight_decay=0.0005)
 
-    # optimzer4nn
-    #optimizer4nn = optim.SGD(model.parameters(), 5)
-    # optimzer4center
-    #optimzer4center = optim.SGD(lgm_loss.parameters(), lr=0.1)
-
-    #sheduler = lr_scheduler.StepLR(optimizer4nn, 20, gamma=0.8)
-
     for epoch in range(100):
 
-        #sheduler.step()
-        # print optimizer4nn.param_groups[0]['lr']
         train(train_loader, model, criterion, optimizer, epoch + 1, loss_weight, use_cuda)
         test(test_loader, criterion, model, use_cuda)
 
 
 if __name__ == '__main__':
+
     main()
