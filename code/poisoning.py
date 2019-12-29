0dbdb559fa5bc5b8db001dc2724db3c85233978b
Muneeb Aadil <maadil@lap-05-410.mpi-inf.mpg.de>
2019-12-29 14:02:44 +0100

    some corrections to poisoning script

--- code/poisoning.py
+++ code/poisoning.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def compute_loss(model, curr_poison, base_img, target_img, beta_zero):
     with torch.no_grad():
-        a = torch.norm(model(curr_poison) - model(target_img))
+        a = torch.norm(model(curr_poison)[0] - \
+            model(target_img)[0])
         b = torch.norm(curr_poison - base_img)
         out = a + beta_zero * b
     return out
@@ -25,13 +26,13 @@ def do_poisoning(target_img, base_img, model, beta=0.25, max_iters=1000,
         poison: PyTorch tensor of shape (1, 3, H, W) containing the poisoned image.
         loss: Loss value of the poisoned image 
     """
-    beta_zero = beta * (2048.0 / base_img.numel) ** 2
+    beta_zero = beta * (2048.0 / base_img.numel()) ** 2
     Lp_func = lambda x: torch.norm(model(x)[1] - model(target_img)[1])
 
     poison = base_img.clone()
     loss = compute_loss(model, poison, base_img, target_img, beta_zero)
     
-    for _ in range(max_iters):
+    for _ in tqdm(range(max_iters)):
         # calculate gradient of Lp w.r.t. x
         poison.requires_grad = True
         norm_val = Lp_func(poison)
@@ -52,6 +53,7 @@ def do_poisoning(target_img, base_img, model, beta=0.25, max_iters=1000,
             # update stuff as final and break out of this optimization.
             poison = new_poison
             loss = new_loss
+            print("Optimization done: Loss = {}".format(loss))
             break
         
         if new_loss > loss: # loss is too big than before; don't update stuff.
@@ -60,6 +62,48 @@ def do_poisoning(target_img, base_img, model, beta=0.25, max_iters=1000,
             poison, loss = new_poison, new_loss
     return poison, loss
 
+
 # testing script
 if __name__ == '__main__':
-    pass
+    import model.net2 as net
+    from torchvision import transforms, datasets
+    from torch.utils.data import DataLoader
+    from tqdm import tqdm
+
+    model = net.Net()
+    model.load_state_dict(torch.load('../testdrivelgm.epoch-39-.model',
+                          map_location='cpu'))
+    # TODO: LOAD THE MODEL WEIGHTS HERE.
+    # model.load_state_dict(state_dict=torch.load("../experiments/"))
+
+    t = transforms.Compose((
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)))
+    )
+    data = datasets.MNIST(
+        root='../datasets/', train=False, download=False, transform=t
+    )
+    loader = DataLoader(data, batch_size=128, shuffle=False, num_workers=4,
+                        pin_memory=False)
+    
+    X, Y = [], []
+    print("Loading dataset into the memory")
+    for (X_, Y_) in tqdm(loader):
+        X.append(X_)
+        Y.append(Y_)
+    
+    X, Y = torch.cat(X, dim=0), torch.cat(Y, dim=0)
+    target_img = X[Y == 4][0] # selecting the first image with label 4.
+    base_img = X[Y == 1][0] # selecting the first iamge with label 1.
+
+    
+    print("Creating poison...")
+    poison, loss = do_poisoning(target_img.unsqueeze(0), 
+                 base_img.unsqueeze(0), model)
+    
+    
+    import matplotlib.pyplot as plt
+    # import pdb
+    # pdb.set_trace()
+    poison_ = poison.squeeze(0).permute(1,2,0)
+    plt.imshow(poison.detach().numpy(), cmap='gray')
