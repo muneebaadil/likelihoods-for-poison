fix normalizing bug

code/get_feat_stats.py
@@ -9,11 +9,17 @@ from tqdm import tqdm
 
 # ===== 
 DEVICE = "cpu"
-CKPT_PATH = "../experiments/softmax_mnist/models/epoch-best.model"
+CKPT_PATH = "../experiments/lgm_mnist/lgm-model"
 DATA_ROOT_PATH = '../datasets/'
 BATCH_SIZE = 1
-METHOD = 'softmax' # "softmax" OR "lgm"
+METHOD = 'lgm' # "softmax" OR "lgm"
 # ===== 
+
+def where(cond, x_1, x_2):
+    out = torch.zeros_like(x_1)
+    out = x_2
+    out[cond] = x_1[cond]
+    return out
 
 use_lgm = True if METHOD == 'lgm' else False
 device = torch.device(DEVICE)
@@ -28,20 +34,30 @@ data = datasets.MNIST(
 )
 loader = DataLoader(data, batch_size=BATCH_SIZE, shuffle=False, num_workers=4,
                     pin_memory=False)
+maxs = torch.tensor([-float("inf"), -float("inf")])
+mins = torch.tensor([float("inf"), float("inf")])
 
-max_feat_val, min_feat_val = None, None
-for (x,y) in tqdm(loader):
-    x = x.to(device)
-    y = y.to(device)
-    
-    _, feats = model(x)
-    _max, _min = feats.max(), feats.min()
+with torch.no_grad():
+    max_feat_val, min_feat_val = None, None
+    for (x,y) in tqdm(loader):
+        x = x.to(device)
+        y = y.to(device)
+        
+        _, feats = model(x)
+        curr_maxs = feats.max(dim=0)[0]
+        curr_mins = feats.min(dim=0)[0]
 
-    if (max_feat_val is None) or (_max > max_feat_val):
-        max_feat_val = _max.item()
-    
-    if (min_feat_val is None) or (_min < min_feat_val):
-        min_feat_val = _min.item()
+        maxs = where(maxs > curr_maxs, maxs, curr_maxs)
+        mins = where(mins < curr_mins, mins, curr_mins)
+        # _max, _min = feats.max(), feats.min()
+
+        # if (max_feat_val is None) or (_max > max_feat_val):
+        #     max_feat_val = _max.item()
+        
+        # if (min_feat_val is None) or (_min < min_feat_val):
+        #     min_feat_val = _min.item()
 
 
-print("Max: {}; Min: {}".format(max_feat_val, min_feat_val))
+# print("Max: {}; Min: {}".format(max_feat_val, min_feat_val))
+print("maxs = {}".format(maxs))
+print("mins = {}".format(mins))

code/poisoning.py
@@ -115,12 +115,12 @@ def get_opts():
 
     # feature scaling.
     if opts.method == 'softmax':
-        opts.feats_max = 334.7419738769531
-        opts.feats_min = -381.4945373535156
+        opts.feats_max = torch.tensor([334.7420, 209.5980])
+        opts.feats_min = torch.tensor([-259.4084, -381.4945])
         opts.feats_range = opts.feats_max - opts.feats_min
     elif opts.method == 'lgm':
-        opts.feats_max = 6.00887393951416
-        opts.feats_min = -6.569829940795898
+        opts.feats_max = torch.tensor([6.0089, 5.3333])
+        opts.feats_min = tensor([-6.5698, -5.4916])
         opts.feats_range = opts.feats_max - opts.feats_min
     else:
         raise NotImplementedError()
@@ -205,7 +205,7 @@ def generate_poison(target_img, base_img, model, logger, beta=0.25, max_iters=10
             if new_loss >= loss: # loss is too big than before; don't update stuff.
                 lr *= decay_coeff
             else: # loss is lower than before and life is good; update stuff.
-                # print("Loss updated")
+                print("Loss updated")
                 poison, loss = new_poison, new_loss
     logger.info("Final Loss = {}".format(loss))
     return poison, loss