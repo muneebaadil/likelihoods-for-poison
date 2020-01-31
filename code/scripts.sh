66d92636862588f65cb9ab90cd67a360128d977e
Muneeb Aadil <imuneebaadil@gmail.com>
2020-01-31 13:37:41 +0100

    add scripts.sh

---
+++ code/scripts.sh
@@ -0,0 +1,6 @@
+# test scalings first for 20 poisons. 
+# bigger beta ==> more importance to feature distance, less importance to image space.
+# smaller beta ==> less important to feature distance, more importance to image space.
+python poisoning.py --dataset mnist --method softmax --ckpt_path ../experiments/softmax_mnist/models/epoch-best.model --base_strategy random --seed 176 --max_poisons 20 --beta 0.00083 --poison_lr 38250000 --normalize_feats --loss_thres 1e-4 --exp_name beta_0_00083
+python poisoning.py --dataset mnist --method softmax --ckpt_path ../experiments/softmax_mnist/models/epoch-best.model --base_strategy random --seed 176 --max_poisons 20 --beta 0.0083 --poison_lr 38250000 --normalize_feats --loss_thres 1e-4 --exp_name beta_0_0083
+python poisoning.py --dataset mnist --method softmax --ckpt_path ../experiments/softmax_mnist/models/epoch-best.model --base_strategy random --seed 176 --max_poisons 20 --beta 0.083 --poison_lr 38250000 --normalize_feats --loss_thres 1e-4 --exp_name beta_0_083
